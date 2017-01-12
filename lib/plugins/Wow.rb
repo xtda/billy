cb05233937e13bf971440011b6aa3acbd93adaf4
Andrew Jackson <andrew@disvelop.net>
2017-01-12 11:01:38 +1100

    first attempt at adding raid progressiong and fixed typo in example config

--- lib/plugins/Wow.rb
+++ lib/plugins/Wow.rb
@@ -30,22 +30,57 @@ module Wow
 
     return event.respond 'Character not found!' if character['status'] == 'nok'
     armory_url = "http://#{region}.battle.net/wow/en/character/#{realm}/#{name}/advanced"
-    event.respond "**Name:** #{character['name']}" \
+    event.respond "**Details:**\n\n" \
+    "**Name:** #{character['name']}" \
     "\n**Class:** #{get_class(character['class'])}" \
     "\n**Faction:** #{get_faction(character['faction'])}" \
     "\n**iLVL:** #{character['items']['averageItemLevelEquipped']} (equipped) / #{character['items']['averageItemLevel']}  (max)" \
-    "\n**Armory**: #{armory_url}"
+    "\n\n**Progression:**\n\n#{get_progression(character)}" \
+    "\n**Armory**: #{armory_url}" 
   end
 
   def self.get_class(value)
     classes = ['Warrior', 'Paladin', 'Hunter', 'Rogue', 'Priest', \
                'Death Knight', 'Shaman', 'Mage', 'Warlock', \
                'Monk', 'Druid', 'Demon Hunter'].freeze
-    classes.at(value - 1)
+    classes.at(value-1)
   end
 
   def self.get_faction(value)
     value.zero? ? 'Alliance' : 'Horde'
   end
 
+  def self.get_progression(input)
+    armory_progression = input['progression']
+    progression = ''
+
+    valid_raids = [
+      'The Emerald Nightmare',
+      'Trial of Valor',
+      'The Nighthold'
+    ]
+
+    armory_progression['raids'].each do |raid|
+      valid_raids.each do |raids|
+        if raid['name'] == raids
+          raid['bosses'].each do |boss|
+            if boss['mythicKills'] >= 1
+              @mythic_progress += 1
+            end
+            if boss['heroicKills'] >= 1
+              @heroic_progress += 1
+            end
+            if boss['normalKills'] >= 1
+              @normal_progress += 1
+            end
+          end
+        progression += "**#{raid['name']}:** (N) #{@normal_progress} / #{raid['bosses'].length} (H) #{@heroic_progress} / #{raid['bosses'].length} (M) #{@mythic_progress} / #{raid['bosses'].length}\n"
+        end
+      end
+      @normal_progress = 0
+      @heroic_progress = 0
+      @mythic_progress = 0
+    end
+    progression
+  end
 end
